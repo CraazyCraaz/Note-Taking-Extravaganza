50a844a81655ae8ed8e422c295cbe32b1d8555c6
Jenny <j.thurnau@hotmail.com>
2020-01-12 12:10:04 -0800

    added comments for future self to look over when restudying

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,27 @@
+// ==============================================================================
+// DEPENDENCIES
+// mysql npm is to connect to Heroku (database server) to send / pull / delete data from
+// ==============================================================================
 var mysql = require("mysql");
-
+// ==============================================================================
+// Creates connection variable to mysql
+// Five (5) standards we're using in class are
+//      host: The hostname of the database you are connecting to. (Default: localhost)
+//      port: The port number to connect to. (Default: 3306)
+//      user: The MySQL user to authenticate as
+//      password: The password of that MySQL user
+//      database: Name of the database to use for this connection (Optional)
+// ==============================================================================
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "notesDB"
-})
-
+});
+// ==============================================================================
+// Function to connect
+// ==============================================================================
 connection.connect(function (err) {
     if (err) {
         console.log(err);
@@ -15,9 +29,16 @@ connection.connect(function (err) {
     }
     console.log("success", connection.threadId);
 
-})
-
+});
+// ==============================================================================
+// API POST Requests
+// Handles when a user hits save and then submits data to the server
+// Submits form data (a JSON object)
+// JSON is pushed to JavaScript array
+// Server saves the data to the tableData array
+// ==============================================================================
 function apiRoutes(app) {
+
     app.post("/api/notes", function (req, res) {
         console.log(req.body)
         connection.query("insert into notes(note_title, note_text_area) values(?,?)", [req.body.note_title, req.body.note_text_area],
@@ -25,8 +46,9 @@ function apiRoutes(app) {
                 res.json(result)
             })
     })
-
-}
-
-
+};
+// ==============================================================================
+// Allows for file to be used in another file
+// Exports info
+// ==============================================================================
 module.exports = apiRoutes
--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-var express = require("express");
-
-var app = express();
-// first part, heroku, second local server
-var PORT = process.env.PORT || 8080;
-
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-app.use(express.static("public"))
-
-var htmlRoutes = require("./routes/htmlRoutes")
-htmlRoutes(app)
-
-var apiRoutes = require("./routes/apiRoutes")
-apiRoutes(app)
-
-
+// ==============================================================================
+// DEPENDENCIES
+// EXPRESS is an npm package used to help with routing and HTTP for localhosting or use with HEROKU
+// Use to give server useful functionality
+// ==============================================================================
+let express = require("express");
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// Tells node that we are creating an "express" server for local hosting or use with HEROKU
+// how to use/call out npm functions in files
+// ==============================================================================
+let app = express();
+// ==============================================================================
+// Sets an initial port, used later in our listener
+// First part is for heroku and second local server
+// ==============================================================================
+let PORT = process.env.PORT || 8080;
+// ==============================================================================
+// Sets up the Express app to handle data parsing
+// Will not work without this, needs it to read files
+// ==============================================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// ==============================================================================
+// To serve static files such as images, CSS files, and JavaScript files
+// The root argument specifies the root directory from which to serve static assets
+// Needed to allow access to the files within "public"
+// ==============================================================================
+app.use(express.static("public"));
+// ==============================================================================
+// ROUTER
+// Points server to a series of "route" files
+// Routes give server a "map" of how to respond when users visit or request data from various URLs
+// ==============================================================================
+let htmlRoutes = require("./routes/htmlRoutes");
+htmlRoutes(app);
+let apiRoutes = require("./routes/apiRoutes");
+apiRoutes(app);
+// =============================================================================
+// LISTENER
+// 'Starts' server
+// =============================================================================
 app.listen(PORT, function () {
   console.log("app listening on port " + PORT);
 
-})
+});
